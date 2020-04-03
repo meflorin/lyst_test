8329b039ce802d5271db4e5ec10d59659bbd67b5
fpaun23 <fpaun23@gmail.com>
2020-04-03 18:54:34 +0300

    refactor

--- cron_sheduler.py
+++ cron_sheduler.py
@@ -14,17 +14,17 @@ def main():
     if (nbArgv > 2):
         cronTime = sys.argv[1]
         for line in fileinput.input(sys.argv[2:]):
-            cronjobs.append(line)         
+            cronjobs.append(line)
+            
+        cronParser = CronShedulerParser(cronjobs, cronTime)
+        cronParser.setDelimiter('/bin')
+        cronParser.processCronJobs()
+        messages = cronParser.getMessages()
+        
+        for i in range(len(cronjobs)):
+            print(cronjobs[i] + messages[i])
+              
     else:
         print('not enough arguments')
-        
-    cronParser = CronShedulerParser(cronjobs, cronTime)
-    cronParser.setDelimiter('/bin')
-    cronParser.processCronJobs()
-    messages = cronParser.getMessages()
-     
-    for i in range(len(cronjobs)):
-        print(cronjobs[i] + messages[i])
-    
     
 main()
